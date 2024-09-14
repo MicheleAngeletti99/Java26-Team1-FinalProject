0cbfa337be2056869ee6e938b14b0bfffdde9e69
Michele Angeletti <m.angeletti99@gmail.com>
2024-09-14 17:37:35 +0200

    Creazione file iniziale

---
+++ src/main/java/com/example/Java26_Team1_FinalProject/Java26Team1FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Java26_Team1_FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java26Team1FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java26Team1FinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Java26_Team1_FinalProject/Java26Team1FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java26_Team1_FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java26Team1FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

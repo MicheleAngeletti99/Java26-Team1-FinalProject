5b22b14c42563cd9999c1e8c6c5aaea608d7cfb9
Michele Angeletti <m.angeletti99@gmail.com>
2024-10-07 16:12:07 +0200

    Fixed documentation

--- src/main/java/com/example/Java26_Team1_FinalProject/controllers/AlbergoController.java
+++ src/main/java/com/example/Java26_Team1_FinalProject/controllers/AlbergoController.java
@@ -23,12 +23,21 @@ public class AlbergoController {
 
     // GET per prendere lenco dei alberghi
     @GetMapping("/elencoalberghi")
+    @Operation(summary = "Lista di tutti gli alberghi", description = "Restituisce una lista di tutti gli alberghi")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tutti gli alberghi sono stati mostrati correttamente")
+    })
     public ResponseEntity<List<Albergo>> elencoAlberghi(){
         return ResponseEntity.ok(albergoService.elencoAlberghi());
     }
 
     // GET per prendere un albergo tramite ID
     @GetMapping("/albergo/{id}")
+    @Operation(summary = "Trova l'albergo per ID", description = "Restituisce un albergo tramite il suo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ID corretto, albergo trovato"),
+            @ApiResponse(responseCode = "404", description = "ID errato, albergo non trovato")
+    })
     public ResponseEntity<Albergo> getAlbergoTramiteId(@PathVariable Long id){
         Optional<Albergo> albergoOptional = albergoService.getAlbergoById(id);
         if(albergoOptional.isPresent()) {
@@ -41,6 +50,11 @@ public class AlbergoController {
 
     // POST per creare un nuovo albergo
     @PostMapping("/newalbergo")
+    @Operation(summary = "Crea un nuovo albergo", description = "Aggiunge un nuovo albergo nel database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Albergo creato correttamente e salvato nel database"),
+            @ApiResponse(responseCode = "400", description = "Richiesta non valida")
+    })
     public ResponseEntity<Albergo> newAlbergo(@RequestBody Albergo albergo){
         Albergo newAlbergo = albergoService.aggiungiAlbergo(albergo);
         return ResponseEntity.ok(newAlbergo);
@@ -48,6 +62,11 @@ public class AlbergoController {
 
     // PUT per modificare
     @PutMapping("/modificaalbergo/{id}")
+    @Operation(summary = "Aggiorna un albergo", description = "Aggiorna le informazioni di un albergo esistente dato il suo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Albergo aggiornato correttamente"),
+            @ApiResponse(responseCode = "404", description = "ID fornito non valido, albergo non trovato")
+    })
     public ResponseEntity<Albergo> modificaalbergo(@PathVariable Long id, @RequestBody Albergo albergo){
         Optional<Albergo> optionalAlbergo = albergoService.modificareAlbergo(id,albergo);
         if(optionalAlbergo.isPresent()){
@@ -57,6 +76,11 @@ public class AlbergoController {
     }
     //DELETE per eliminare
     @DeleteMapping("/eliminareAlbergo/{id}")
+    @Operation(summary = "Elimina un albergo", description = "Elimina un albergo dal database dato il suo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Albergo trovato tramite ID ed eliminato correttamente"),
+            @ApiResponse(responseCode = "404", description = "ID non presente nel database")
+    })
     public ResponseEntity<Void> eliminareAlbergo(@PathVariable Long id){
         boolean rimuoviAlbergo = albergoService.rimuovereAlbergo(id);
           if(rimuoviAlbergo){
@@ -67,6 +91,11 @@ public class AlbergoController {
 
     // POST per aggiungere un servizio alla lista specificato dal Albergo
     @PostMapping("/servizi/{idAlbergo}/{servizio}")
+    @Operation(summary = "Aggiunge un servizio all'albergo", description = "Aggiunge un servizio alla lista dei servizi messi a disposizione dall'albergo di cui viene fornito l'id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Servizio aggiunto correttamente alla lista dell'albergo"),
+            @ApiResponse(responseCode = "404", description = "ID non presente nel database")
+    })
     public ResponseEntity<String> aggiungiServizi(@PathVariable Long  idAlbergo,@PathVariable Integer  servizio){
        boolean addServizio =  albergoService.aggiungiServizio(idAlbergo,servizio);
        if(addServizio){
@@ -76,6 +105,11 @@ public class AlbergoController {
     }
     // DELETE per rimuovere un servizio  specificato dal Albergo
     @DeleteMapping("/servizi/{idAlbergo}/{servizio}")
+    @Operation(summary = "Rimuove un servizio all'albergo", description = "Rimuove un servizio dalla lista dei servizi messi a disposizione dall'albergo di cui viene fornito l'id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Servizio rimosso correttamente dalla lista dell'albergo"),
+            @ApiResponse(responseCode = "404", description = "ID non presente nel database")
+    })
     public ResponseEntity<String>  rimuoviServizio(@PathVariable Long idAlbergo,@PathVariable Integer servizio){
         boolean rimuoviServizio = albergoService.eliminaServizio(idAlbergo,servizio);
         if(rimuoviServizio){
--- src/main/java/com/example/Java26_Team1_FinalProject/controllers/EnteController.java
+++ src/main/java/com/example/Java26_Team1_FinalProject/controllers/EnteController.java
@@ -1,6 +1,9 @@
 package com.example.Java26_Team1_FinalProject.controllers;
 import com.example.Java26_Team1_FinalProject.entities.Ente;
 import com.example.Java26_Team1_FinalProject.services.EnteServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +20,21 @@ public class EnteController {
 
     //get per prendere la lsita di enti
     @GetMapping("/elenco-enti")
+    @Operation(summary = "Lista di tutti gli enti", description = "Restituisce una lista di tutti gli enti")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tutti gli enti sono stati mostrati correttamente")
+    })
     public ResponseEntity<List<Ente>> elencoEnti(){
         return ResponseEntity.ok(enteServices.elencoEnti());
     }
 
     //post per creare un ente
     @PostMapping("/new-ente")
+    @Operation(summary = "Crea un nuovo ente", description = "Aggiunge un nuovo ente nel database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Ente creato correttamente e salvato nel database"),
+            @ApiResponse(responseCode = "400", description = "Richiesta non valida")
+    })
     public ResponseEntity<Ente> newEnte(@RequestBody Ente ente){
         Ente addEnte = enteServices.addEnte(ente);
         return ResponseEntity.ok(addEnte);
@@ -30,6 +42,11 @@ public class EnteController {
 
     //delete del ente
     @DeleteMapping("/eliminare-ente/{id}")
+    @Operation(summary = "Elimina un'ente", description = "Elimina un'ente dal database dato il suo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Ente trovato tramite ID ed eliminato correttamente"),
+            @ApiResponse(responseCode = "404", description = "ID non presente nel database")
+    })
     public ResponseEntity<Void> eliminareEnte(@PathVariable Long id){
         boolean rimuoviAlbergo = enteServices.deleteEnte(id);
         if(rimuoviAlbergo){
@@ -40,6 +57,11 @@ public class EnteController {
 
     //modifica ente
     @PutMapping("/modifica-ente/{id}")
+    @Operation(summary = "Aggiorna un'ente", description = "Aggiorna le informazioni di un'ente esistente dato il suo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ente aggiornato correttamente"),
+            @ApiResponse(responseCode = "404", description = "ID fornito non valido, ente non trovato")
+    })
     public ResponseEntity<Ente> modificaEnte(@PathVariable Long id, @RequestBody Ente ente){
         Optional<Ente> enteOptional = enteServices.modificaEnte(id,ente);
         if (enteOptional.isPresent()){
@@ -49,6 +71,11 @@ public class EnteController {
     }
 
     @GetMapping("/ente/{id}")
+    @Operation(summary = "Trova l'ente per ID", description = "Restituisce un'ente tramite il suo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ID corretto, ente trovato"),
+            @ApiResponse(responseCode = "404", description = "ID errato, ente non trovato")
+    })
     public ResponseEntity<Ente> getEnteById(@PathVariable Long id){
         Optional<Ente> enteOptional = enteServices.getEnteById(id);
         if(enteOptional.isPresent()) {
